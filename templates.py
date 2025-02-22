299604d834d7966b05beafb4fe0721f711a95767
Vasques <76536544+NunoVz@users.noreply.github.com>
2025-02-22 18:11:31 +0000

    Templates

--- templates.py
+++ templates.py
@@ -1,17 +1,37 @@
 import requests
+from tabulate import tabulate
 
 XO_URL = "http://xo-cslab.dei.uc.pt"
+endpoint = "/rest/v0/vm-templates"
+url = XO_URL + endpoint
 token = "Aty79-OVxXiGY40-eCnWqq0YpeBsMZG-lZn73yec4H0"
 cookies = {'authenticationToken': token}
 
-def make_vm_template(vm_id):
-    url = f"{XO_URL}/rest/v0/vms/{vm_id}/actions/make_template"
-    response = requests.post(url, cookies=cookies)
-    print("Make Template Status Code:", response.status_code)
-    try:
-        print("Response:", response.json())
-    except Exception as e:
-        print("Error parsing response:", response.text)
+response = requests.get(url, cookies=cookies)
+try:
+    data = response.json()
+except Exception as e:
+    print("Error decoding JSON:", response.text)
+    exit(1)
 
-vm_id = "6365ed62-0c96-2465-ea9c-a70ed9832200"  
-make_vm_template(vm_id)
+table_data = []
+
+# Assuming the endpoint returns a dictionary where each key is the template UUID
+if isinstance(data, dict):
+    for template_uuid, details in data.items():
+        name = details.get("name_label", "N/A")
+        table_data.append([template_uuid, name])
+elif isinstance(data, list):
+    # Alternatively, if the endpoint returns a list of template objects
+    for item in data:
+        uuid = item.get("id", "N/A")
+        name = item.get("name_label", "N/A")
+        table_data.append([uuid, name])
+else:
+    print("Unexpected data structure:", type(data))
+    exit(1)
+
+if table_data:
+    print(tabulate(table_data, headers=["Template UUID", "Name"], tablefmt="pretty"))
+else:
+    print("No templates found.")
