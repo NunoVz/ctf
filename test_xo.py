20a810462f1cb316631c98f84df6cb0d640d1350
Vasques <76536544+NunoVz@users.noreply.github.com>
2025-02-22 14:42:16 +0000

    Request

--- test_xo.py
+++ test_xo.py
@@ -9,16 +9,17 @@ XO_URL = "http://xo-cslab.dei.uc.pt"
 token = "Aty79-OVxXiGY40-eCnWqq0YpeBsMZG-lZn73yec4H0"
 cookies = {'authenticationToken': token}
 
-# Get VMs from Xen Orchestra
+
 response = requests.get(f"{XO_URL}/rest/v0/vms", cookies=cookies)
-vms = response.json()
+vm_ids = response.json()
 
 table_data = []
-for vm in vms:
-    vm_id = vm.get('id', 'N/A')
-    name = vm.get('name_label', 'N/A')
-    description = vm.get('name_description', 'N/A')
-    table_data.append([vm_id, name, description])
 
+for vm_id in vm_ids:
+    detail_response = requests.get(f"{XO_URL}/rest/v0/vms/{vm_id}", cookies=cookies)
+    vm_details = detail_response.json()
+    name = vm_details.get('name_label', 'N/A')
+    description = vm_details.get('name_description', 'N/A')
+    table_data.append([vm_id, name, description])
 
 print(tabulate(table_data, headers=["VM ID", "Name", "Description"], tablefmt="pretty"))
