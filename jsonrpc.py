5e788ca48e8f67f2525dad7a36a2b15b87eb3b04
Vasques <76536544+NunoVz@users.noreply.github.com>
2025-02-24 14:18:04 +0000

    JsonRpc

--- jsonrpc.py
+++ jsonrpc.py
@@ -2,7 +2,7 @@ import requests
 from tabulate import tabulate
 
 # JSON-RPC endpoint
-JSON_RPC_URL = "http://xo-cslab.dei.uc.pt/jsonrpc"
+JSON_RPC_URL = "http://xo-cslab.dei.uc.pt/api"
 
 # Replace these with your actual credentials
 USERNAME = "cslab"
